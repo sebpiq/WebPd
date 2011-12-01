90c94246bb49a8032a9dd23470b647a1fb393c95
Chris McCormick <chrism@mccormix.xen.prgmr.com>
2011-12-01 12:13:34 +0800

    Analytics.

--- index.php
+++ index.php
@@ -24,5 +24,18 @@
 </pre>
 </div>
 
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.mccormick.cx/" : "http://stats.mccormick.cx/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://stats.mccormick.cx/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
+
 </body>
 </html>
