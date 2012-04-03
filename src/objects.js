aa8db1892b2680518fdcd18f4701f49698ffa35e
Sébastien Piquemal <sebastien.piquemal@futurice.com>
2012-04-03 13:34:26 +0300

    optimization to tabwrite~

--- src/objects.js
+++ src/objects.js
@@ -7,6 +7,8 @@
 	(Basically if you provide this software via the network you need to make the source code available, but read the license for details).
 ***/
 
+// TODO: message should take an array, instead of having to parse everything themselves.
+
 (function(Pd){
 
 
@@ -117,7 +119,104 @@
 
 	});
 
-	
+
+	// digital to analogue converter (sound output)
+	Pd.objects['dac~'] = Pd.Object.extend({
+
+		endPoint: true,
+		inletTypes: ['inlet~', 'inlet~'],
+
+		dspTick: function() {
+			var inBuff1 = this.inlets[0].getBuffer();
+			var inBuff2 = this.inlets[1].getBuffer();
+            var output = this.getPatch().output;
+			// copy interleaved data from inlets to the graph's output buffer
+			for (var i=0; i < Pd.blockSize; i++) {
+				output[i * 2] += inBuff1[i];
+				output[i * 2 + 1] += inBuff2[i];
+			}
+		}
+	});
+
+
+	// creates simple dsp lines
+	Pd.objects['line~'] = Pd.Object.extend(StopEventMixin, {
+
+		inletTypes: ['inlet'],
+		outletTypes: ['outlet~'],
+
+		init: function() {
+            StopEventMixin.init.call(this);
+			// what the value was at the start of the line
+			this.y0 = 0;
+			// the destination value we are aiming for
+			this.y1 = 0;
+            // this stores the current index 
+            this.n = 0;
+			// this stores the index max the line must reach
+			this.nMax = 0;
+			// we want to use the dsptick method that returns a constant value for now
+			this.toDspConst(this.y0);
+		},
+
+		// write a constant value to our output buffer for every sample
+		dspTickConst: function() {
+            var outBuff = this.outlets[0].getBuffer();
+			for (var i=0; i<outBuff.length; i++) outBuff[i] = this.y0;
+		},
+
+		// write this correct value of the line at each sample
+		dspTickLine: function() {
+            var outBuff = this.outlets[0].getBuffer();
+            var outBuffLength = outBuff.length;
+            var slope = this.slope;
+			for (var i=0; i<outBuffLength; i++, this.n++) {
+				// if we've reached the end of our line, we fill-in the rest of the buffer,
+                // break, and switch back to the constant method.
+				if (this.n >= this.nMax) {
+                    for (var j=i; j<outBuffLength; j++) outBuff[j] = this.y1;
+                    this.toDspConst(this.y1);
+                    this._execOnStop();
+                    break;
+				} else {
+					outBuff[i] = this.n * slope + this.y0;
+				}
+			}
+		},
+
+		message: function(inletId, message) {
+			if (inletId == 0) {
+				var parts = this.toArray(message);
+				// if this is a single valued message we want line~ to output a constant value,
+                // otherwise the message is taken as [targetY duration(
+				if (parts.length == 1) {
+					var y0 = this.toFloat(parts[0]);
+					if (!isNaN(y0)) this.toDspConst(y0);
+				} else if (parts.length >= 2){
+					var y1 = this.toFloat(parts[0]);
+					var duration = this.toFloat(parts[1]);
+					if (!isNaN(y1) && !isNaN(duration)) this.toDspLine(y1, duration)
+				}
+			}
+		},
+
+        toDspConst: function(val) {
+            this.y0 = val;
+			this.dspTick = this.dspTickConst;
+        },
+
+        toDspLine: function(val, duration) {
+			this.y1 = val;
+            this.n = 0;
+			this.nMax = duration * this.getPatch().getSampleRate() / 1000;
+            this.slope = (this.y1 - this.y0) / this.nMax;
+			this.dspTick = this.dspTickLine;
+        }
+	});	
+
+
+/************************** DSP arithmetics ******************************/
+
     var DSPArithmBase = Pd.Object.extend({
 
 		inletTypes: ['inlet~', 'inlet~'],
@@ -202,6 +301,7 @@
 
 	});
 
+
 	// dsp substraction object
 	Pd.objects['-~'] = DSPArithmBase.extend({
 
@@ -222,24 +322,7 @@
 
 	});
 
-	// digital to analogue converter (sound output)
-	Pd.objects['dac~'] = Pd.Object.extend({
-
-		endPoint: true,
-		inletTypes: ['inlet~', 'inlet~'],
-
-		dspTick: function() {
-			var inBuff1 = this.inlets[0].getBuffer();
-			var inBuff2 = this.inlets[1].getBuffer();
-            var output = this.getPatch().output;
-			// copy interleaved data from inlets to the graph's output buffer
-			for (var i=0; i < Pd.blockSize; i++) {
-				output[i * 2] += inBuff1[i];
-				output[i * 2 + 1] += inBuff2[i];
-			}
-		}
-	});
-
+/************************** DSP tables ******************************/
 
     // Baseclass for tabwrite~, tabread~ and others ...
     var DSPTabBase = Pd.Object.extend({
@@ -261,8 +344,13 @@
                 if (!table) throw (new Error('table with name ' + name + ' doesn\'t exist'));
                 this.table = table;
             }
-        }
+        },
+
+        dspTickNoOp: function() {},
+
+        toDspTickNoOp: function() { this.dspTick = this.dspTickNoOp; }
     });
+
 
     // read data from a table with no interpolation
     Pd.objects['tabread~'] = DSPTabBase.extend({
@@ -297,6 +385,55 @@
     });
 
 
+    // play data from a table with no interpolation
+    Pd.objects['tabplay~'] = DSPTabBase.extend(StopEventMixin, {
+
+        inletTypes: ['inlet'],
+        outletTypes: ['outlet~'],
+
+        init: function(tableName) {
+            DSPTabBase.prototype.init.call(this, tableName);
+            StopEventMixin.init.call(this);
+            this.pos = 0;
+            this.maxPos = 0;
+            this.toDspTickNoOp();
+        },
+
+        dspTickReading: function() {
+            var outBuff = this.outlets[0].getBuffer();
+            
+        },
+
+        message: function(inletId, msg) {
+			if (inletId === 0) {
+                var parts = this.toArray(msg);
+                var method = parts[0];
+                if (method == 'set') {
+                    this.setTableName(parts[1]);
+                    this.toDspTickNoOp();
+                } else if (msg == 'bang') {
+                    this.toDspTickWriting(0);
+                } else if (parts.length == 1) {
+                    var startPos = this.toFloat(parts[0]);
+                    this.toDspTickReading(startPos);
+                } else if (parts.length == 2) {
+                    var startPos = this.toFloat(parts[0]);
+                    var sampleNum = this.toFloat(parts[1]);
+                    this.toDspTickReading(startPos, sampleNum);
+                }
+            }
+        },
+
+        toDspTickReading: function(startPos, sampleNum) {
+            var max
+            if (startPos >= this.table.size - 1) return;
+            this.pos = sampleNum;
+            this.maxPos = Math.max(startPos + sampleNum, this.table.size - 1);
+        }
+
+    });
+
+
     // read data from a table with no interpolation
     Pd.objects['tabwrite~'] = DSPTabBase.extend(StopEventMixin, {
 
@@ -312,18 +449,16 @@
 
         dspTickWriting: function() {
             var inBuff = this.inlets[0].getBuffer();
-            var tableSize = this.table.size;
-            for (var i=0; i<inBuff.length; i++, this.pos++) {
+            var maxPos = Math.min(inBuff.length, this.table.size - this.pos);
+            for (var i=0; i<maxPos; i++, this.pos++) {
                 this.table.data[this.pos] = inBuff[i];
-                if (this.pos == tableSize) {
-                    this.toDspTickNoOp();
-                    this._execOnStop();
-                    break;
-                }
+            }
+            // If we reached table size, that's it
+            if (this.pos == this.table.size) {
+                this.toDspTickNoOp();
+                this._execOnStop();
             }
         },
-
-        dspTickNoOp: function() {},
 
         message: function(inletId, msg) {
 			if (inletId === 0) {
@@ -348,89 +483,11 @@
             }
         },
 
-        toDspTickNoOp: function() { this.dspTick = this.dspTickNoOp; },
-
         toDspTickWriting: function(start) { 
             this.dspTick = this.dspTickWriting;
             this.pos = start;
         }
     });
-
-
-	// creates simple dsp lines
-	Pd.objects['line~'] = Pd.Object.extend(StopEventMixin, {
-
-		inletTypes: ['inlet'],
-		outletTypes: ['outlet~'],
-
-		init: function() {
-            StopEventMixin.init.call(this);
-			// what the value was at the start of the line
-			this.y0 = 0;
-			// the destination value we are aiming for
-			this.y1 = 0;
-            // this stores the current index 
-            this.n = 0;
-			// this stores the index max the line must reach
-			this.nMax = 0;
-			// we want to use the dsptick method that returns a constant value for now
-			this.toDspConst(this.y0);
-		},
-
-		// write a constant value to our output buffer for every sample
-		dspTickConst: function() {
-            var outBuff = this.outlets[0].getBuffer();
-			for (var i=0; i<outBuff.length; i++) outBuff[i] = this.y0;
-		},
-
-		// write this correct value of the line at each sample
-		dspTickLine: function() {
-            var outBuff = this.outlets[0].getBuffer();
-            var outBuffLength = outBuff.length;
-            var slope = this.slope;
-			for (var i=0; i<outBuffLength; i++, this.n++) {
-				// if we've reached the end of our line, we fill-in the rest of the buffer,
-                // break, and switch back to the constant method.
-				if (this.n >= this.nMax) {
-                    for (var j=i; j<outBuffLength; j++) outBuff[j] = this.y1;
-                    this.toDspConst(this.y1);
-                    this._execOnStop();
-                    break;
-				} else {
-					outBuff[i] = this.n * slope + this.y0;
-				}
-			}
-		},
-
-		message: function(inletId, message) {
-			if (inletId == 0) {
-				var parts = this.toArray(message);
-				// if this is a single valued message we want line~ to output a constant value,
-                // otherwise the message is taken as [targetY duration(
-				if (parts.length == 1) {
-					var y0 = this.toFloat(parts[0]);
-					if (!isNaN(y0)) this.toDspConst(y0);
-				} else if (parts.length >= 2){
-					var y1 = this.toFloat(parts[0]);
-					var duration = this.toFloat(parts[1]);
-					if (!isNaN(y1) && !isNaN(duration)) this.toDspLine(y1, duration)
-				}
-			}
-		},
-
-        toDspConst: function(val) {
-            this.y0 = val;
-			this.dspTick = this.dspTickConst;
-        },
-
-        toDspLine: function(val, duration) {
-			this.y1 = val;
-            this.n = 0;
-			this.nMax = duration * this.getPatch().getSampleRate() / 1000;
-            this.slope = (this.y1 - this.y0) / this.nMax;
-			this.dspTick = this.dspTickLine;
-        }
-	});
 
 
 /************************** Misc non-DSP ******************************/
