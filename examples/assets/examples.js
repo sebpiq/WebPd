fc2c0138d4ff1584fa611a8f1df09cb7a52482bb
Sébastien Piquemal <sebpiq@gmail.com>
2015-10-18 20:17:29 +0300

    Added special case for iOS in examples to solve #81

--- examples/assets/examples.js
+++ examples/assets/examples.js
@@ -1,7 +1,12 @@
 webPdExamples = {
   
   init: function() {
-    $('#startButton').click(function() {
+    // starting in iOS9, audio will only be unmuted if the context is created on "touchend".
+    // see : https://github.com/sebpiq/WebPd/issues/81  
+    var is_iOS = /iPad|iPhone|iPod/.test(navigator.platform)
+      , eventType = is_iOS ? 'touchend' : 'click'
+    
+    $('#startButton').on(eventType, function() {
       $(this).fadeOut(200, function() { $('#controls').fadeIn(200) })
       Pd.start()
     })
