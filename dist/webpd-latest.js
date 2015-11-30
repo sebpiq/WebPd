sebpiq/WebPd

commit fe9d61d7f2f672986727af9c56a7330018d02b06

handles graphs as normal subpatches fixes #88

index.js
@@ -159,23 +159,19 @@ var Pd = module.exports = {
       var proto = nodeData.proto
         , obj
       
-      if (proto === 'graph') {
-        var arrayNodeData = nodeData.subpatch.nodes[0]
-        obj = patch._createObject('array', arrayNodeData.args || [])
-        obj.setData(new Float32Array(arrayNodeData.data), true)
-        proto = 'array'
-
-      } else {
-        try {
-          obj = patch._createObject(proto, nodeData.args || [])
-        } catch (err) {
-          if (err instanceof errors.UnkownObjectError) 
-            errorList.push([ err.message, err ])
-          else throw err
-        }
+      try {
+        obj = patch._createObject(proto, nodeData.args || [])
+      } catch (err) {
+        if (err instanceof errors.UnkownObjectError) 
+          return errorList.push([ err.message, err ])
+        else throw err
       }
       
-      if (proto === 'pd') Pd._preparePatch(obj, nodeData.subpatch)
+      if (obj.type == 'array')
+        obj.setData(new Float32Array(nodeData.data), true)
+
+      if (proto === 'pd' || proto === 'graph') 
+        Pd._preparePatch(obj, nodeData.subpatch)
       createdObjs[nodeData.id] = obj
     })
 

lib/glue.js
@@ -983,6 +983,6 @@ exports.declareObjects = function(library) {
 
   })
 
-  library['pd'] = Patch
+  library['pd'] = library['graph'] = Patch
 
 }

test/lib/Pd-tests.js
@@ -229,6 +229,11 @@ describe('Pd', function() {
       assert.equal(startCalled, 1)
     })
 
+    it('should load patch with graph', function() {
+      var patchStr = fs.readFileSync(__dirname + '/patches/graph.pd').toString()
+        , patch = Pd.loadPatch(patchStr)
+    })
+
     it('should throw PatchLoadError if unknown objects', function() {
       var patchData = {
           nodes: [ 

test/lib/glue-tests.js
@@ -1329,7 +1329,8 @@ describe('glue', function() {
       }) // Just because not available ATM
       var patchStr = fs.readFileSync(path.resolve(__dirname, 'patches', 'array-saved-data.pd'))
         , patch = Pd.loadPatch(patchStr.toString())
-        , array = _.find(patch.objects, function(obj) { return obj.type === 'array' })
+        , graph = _.find(patch.objects, function(obj) { return obj.type === 'patch' })
+        , array = _.find(graph.objects, function(obj) { return obj.type === 'array' })
       assert.deepEqual(array.data, new Float32Array([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8]))
       assert.equal(array.name, 'BLA')
     })

test/lib/patches/graph.pd
@@ -1,0 +1,4 @@
+#N canvas 49 82 450 300 10;
+#N canvas 0 0 450 300 (subpatch) 0;
+#X coords 0 1 100 -1 200 140 1;
+#X restore 100 20 graph;

commit b4e2226d8eb3612c8fdc94f32ef54441a3085f9b

fixed small bug introduced in fe9d61d7f2f672986727af9c56a7330018d02b06

dist/webpd-latest.js
@@ -350,8 +350,8 @@ var Pd = module.exports = {
           return errorList.push([ err.message, err ])
         else throw err
       }
-      
-      if (obj.type == 'array')
+
+      if (obj.type == 'array' && nodeData.data)
         obj.setData(new Float32Array(nodeData.data), true)
 
       if (proto === 'pd' || proto === 'graph') 

index.js
@@ -166,8 +166,8 @@ var Pd = module.exports = {
           return errorList.push([ err.message, err ])
         else throw err
       }
-      
-      if (obj.type == 'array')
+
+      if (obj.type == 'array' && nodeData.data)
         obj.setData(new Float32Array(nodeData.data), true)
 
       if (proto === 'pd' || proto === 'graph') 