4187b24b0f3ece9e8ed45a4a81c1b6b2530647cc
Sébastien Piquemal <sebpiq@protonmail.com>
2023-03-10 16:11:59 +0100

    added license headers

--- src/nodes/nodes-shared-code/core.ts
+++ src/nodes/nodes-shared-code/core.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2022-2023 Sébastien Piquemal <sebpiq@protonmail.com>, Chris McCormick.
+ *
+ * This file is part of WebPd 
+ * (see https://github.com/sebpiq/WebPd).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 import { SharedCodeGenerator } from "@webpd/compiler-js/src/types";
 
 export const bangUtils: SharedCodeGenerator = ({ macros: { Func, Var }}) => `
--- src/nodes/nodes/binop.ts
+++ src/nodes/nodes/binop.ts
@@ -1,12 +1,21 @@
 /*
- * Copyright (c) 2012-2020 Sébastien Piquemal <sebpiq@gmail.com>
+ * Copyright (c) 2022-2023 Sébastien Piquemal <sebpiq@protonmail.com>, Chris McCormick.
  *
- * BSD Simplified License.
- * For information on usage and redistribution, and for a DISCLAIMER OF ALL
- * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ * This file is part of WebPd 
+ * (see https://github.com/sebpiq/WebPd).
  *
- * See https://github.com/sebpiq/WebPd_pd-parser for documentation
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import {
